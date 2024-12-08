016cd74936e708c2ebe9d8a1f2cde3a550e398f6
Stanislav <stas.litovchuk@mail.ru>
2024-12-08 13:38:29 +0300

    Done 3rd task

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.average_grades()
 
     def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return "Ошибка"
+
+    def average_grades(self):
+        avr_grades = 0
+        count = 0
+        for grades in self.grades.values():
+            for grade in grades:
+                avr_grades += grade
+                count += 1
+            return avr_grades / count
+
+    def __str__(self):
+        return f'Имя: {self.name}\n'\
+                f'Фамилия: {self.surname}\n'\
+                f'Средняя оценка за домашние задания: {self.average_grades()}\n'\
+                f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'\
+                f'Завершенные курсы: {", ".join(self.finished_courses)}'
+
+    def __eq__(self, other):
+        return isinstance(other, Student) and self.average_grades() == other.average_grades()
 
 class Mentor:
     def __init__(self, name, surname):
@@ -24,8 +44,24 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    def __init__(self):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
         self.grades = {}
+
+    def average_grades(self):
+        avr_grades = 0
+        count = 0
+        for grades in self.grades.values():
+            for grade in grades:
+                avr_grades += grade
+                count += 1
+            return avr_grades / count
+
+    def __str__(self):
+        return f'Имя: {self.name}\n'f'Фамилия: {self.surname}'
+
+    def __eq__(self, other):
+        return isinstance(other, Lecturer) and self.average_grades() == other.average_grades()
 
 
 class Reviewer(Mentor):
@@ -38,15 +74,42 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'Имя: {self.name}\n'f'Фамилия: {self.surname}\n'
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+
+some_student = Student('Ruoy', 'Eman')
+some_student1 = Student('Gaibe', 'Name')
+
+
+some_student.courses_in_progress += ['Python']
+some_student1.courses_in_progress += ['C++']
+
+
+some_student.finished_courses += ["C++"]
+some_student.finished_courses += ["Java"]
+
+some_lecturer1 = Lecturer("Go", "Ahead")
+
+some_lecturer = Lecturer("Some", "Buddy")
+
+some_lecturer.courses_attached = ['Python']
+some_lecturer1.courses_attached = ['C++']
+
+some_student.rate_hw(some_lecturer, 'Python', 10)
+some_student.rate_hw(some_lecturer1, 'C++', 10)
+
+some_reviewer = Reviewer("Some", "Buddy")
+some_reviewer.courses_attached += ['Python']
+some_reviewer.courses_attached += ['C++']
+
+
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student1, 'C++', 5)
+# print(some_lecturer)
+# print(some_reviewer)
+# print(some_student)
